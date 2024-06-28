06e3982379abb3249cc562334fcc754a4eb318d2
Raz Hagay Gavriel <raz5338@gmail.com>
2024-06-28 04:48:55 +0300

    Add close menu function after choosing menu option

--- client/src/App/AuthApp/components/AppBar/UserMenu.jsx
+++ client/src/App/AuthApp/components/AppBar/UserMenu.jsx
@@ -1,4 +1,4 @@
-import { LightMode, Logout, ModeNight, LocalDrinkRounded, PlusOne }  from '@mui/icons-material';
+import { LightMode, Logout, ModeNight, PlusOne } from '@mui/icons-material';
 import GroupsIcon from '@mui/icons-material/Groups';
 import { Avatar, IconButton, ListItemIcon, Menu, MenuItem, Tooltip, useTheme } from '@mui/material';
 import { useQueryClient } from '@tanstack/react-query';
@@ -9,93 +9,96 @@ import { useColorMode } from '../../../../Providers/ThemeProvider';
 import { useUser } from '../../../../Providers/UserProvider';
 import { logoutUserFn } from '../../../../axios/auth';
 
-const getInitials = fullName => fullName === ''? '' : fullName.trim().split(/\s+/).map(name => name[0].toUpperCase()).join('').slice(0, 3);
+const getInitials = fullName => fullName === '' ? '' : fullName.trim().split(/\s+/).map(name => name[0].toUpperCase()).join('').slice(0, 3);
 
 const UserMenu = () => {
-    const [anchorEl, setAnchorEl] = useState(null);
-    const navigate = useNavigate();
-    const { toggleColorMode } = useColorMode();
-    const { palette: { mode } } = useTheme();
-    const { user, setUser } = useUser();
-    const queryCache = useQueryClient().getQueryCache();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const navigate = useNavigate();
+  const { toggleColorMode } = useColorMode();
+  const { palette: { mode } } = useTheme();
+  const { user, setUser } = useUser();
+  const queryCache = useQueryClient().getQueryCache();
 
-    const logoutUser = async () => {
-        try {
-            await logoutUserFn();
-        }
-        catch (e) {
-            console.log(e.message)
-        }
-        
-        localStorage.removeItem('token');
-        localStorage.removeItem('user');
-        setUser(null);
-        queryCache.clear();
+  const logoutUser = async () => {
+    try {
+      await logoutUserFn();
+    } catch (e) {
+      console.log(e.message);
+    }
 
-        navigate('/login');
-    };
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    setUser(null);
+    queryCache.clear();
 
-    const openMenu = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
+    navigate('/login');
+    closeMenu();
+  };
 
-    const closeMenu = () => {
-        setAnchorEl(null);
-    };
+  const openMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const navigateToUserProfile = () => {
-        navigate('/profile');
-    };
+  const closeMenu = () => {
+    setAnchorEl(null);
+  };
 
-    const navigateToAddPurchaseGroupPage = () => {
-        navigate('/add-purchase-group');
-    };
+  const navigateToUserProfile = () => {
+    navigate('/profile');
+    closeMenu();
+  };
 
-    const navigateToPurchaseGroupFeed = () => {
-        navigate('/purchase-groups-feed');
-    };
+  const navigateToAddPurchaseGroupPage = () => {
+    navigate('/add-purchase-group');
+    closeMenu();
+  };
 
-    return (
-        <>
-            <Tooltip title="Account settings">
-                <IconButton onClick={openMenu}>
-                    <Avatar sx={{ bgcolor: stringToColor(user?.name) }}>{getInitials(user?.name)}</Avatar>
-                </IconButton>
-            </Tooltip>
-            <Menu open={Boolean(anchorEl)} anchorEl={anchorEl} onClose={closeMenu}>
-                <MenuItem onClick={navigateToUserProfile}>
-                <ListItemIcon style={{ marginRight: '3px' }}>
-                        {<Avatar/>}
-                    </ListItemIcon>
-                    Profile
-                </MenuItem>
-                <MenuItem onClick={navigateToAddPurchaseGroupPage}>
-                <ListItemIcon style={{ marginRight: '3px' }}>
-                        {<PlusOne/>}
-                    </ListItemIcon>
-                    Add purchase group
-                </MenuItem>
-                <MenuItem onClick={navigateToPurchaseGroupFeed}>
-                <ListItemIcon style={{ marginRight: '3px' }}>
-                        {<GroupsIcon/>}
-                    </ListItemIcon>
-                    Purchase groups
-                </MenuItem>
-                <MenuItem onClick={toggleColorMode}>
-                    <ListItemIcon>
-                        {mode === 'light' ? <LightMode fontSize="small" /> : <ModeNight fontSize="small" />}
-                    </ListItemIcon>
-                    Change Color Mode
-                </MenuItem>
-                <MenuItem onClick={async () => {await logoutUser()}}>
-                    <ListItemIcon>
-                        <Logout fontSize="small" />
-                    </ListItemIcon>
-                    Logout
-                </MenuItem>
-            </Menu>
-        </>
-    );
+  const navigateToPurchaseGroupFeed = () => {
+    navigate('/purchase-groups-feed');
+    closeMenu();
+  };
+
+  return (
+    <>
+      <Tooltip title="Account settings">
+        <IconButton onClick={openMenu}>
+          <Avatar sx={{ bgcolor: stringToColor(user?.name) }}>{getInitials(user?.name)}</Avatar>
+        </IconButton>
+      </Tooltip>
+      <Menu open={Boolean(anchorEl)} anchorEl={anchorEl} onClose={closeMenu}>
+        <MenuItem onClick={navigateToUserProfile}>
+          <ListItemIcon style={{ marginRight: '3px' }}>
+            <Avatar />
+          </ListItemIcon>
+          Profile
+        </MenuItem>
+        <MenuItem onClick={navigateToAddPurchaseGroupPage}>
+          <ListItemIcon style={{ marginRight: '3px' }}>
+            <PlusOne />
+          </ListItemIcon>
+          Add purchase group
+        </MenuItem>
+        <MenuItem onClick={navigateToPurchaseGroupFeed}>
+          <ListItemIcon style={{ marginRight: '3px' }}>
+            <GroupsIcon />
+          </ListItemIcon>
+          Purchase groups
+        </MenuItem>
+        <MenuItem onClick={toggleColorMode}>
+          <ListItemIcon>
+            {mode === 'light' ? <LightMode fontSize="small" /> : <ModeNight fontSize="small" />}
+          </ListItemIcon>
+          Change Color Mode
+        </MenuItem>
+        <MenuItem onClick={logoutUser}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </>
+  );
 };
 
 export default UserMenu;
