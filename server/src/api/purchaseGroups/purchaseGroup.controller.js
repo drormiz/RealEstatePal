1401c1e0d2e47d1be68ba7db76ac02937b034c01
RazGavrielColman <raz5338@cs.colman.ac.il>
2024-07-24 13:23:03 +0300

    Add ViewPurchaseGroup component functionality for displaying and joining purchase groups and view purchase requests

--- server/src/api/purchaseGroups/purchaseGroup.controller.js
+++ server/src/api/purchaseGroups/purchaseGroup.controller.js
@@ -1,68 +1,267 @@
-import { PurchaseGroupModel } from '../../models/purchaseGroup.model.js';
+import { PurchaseGroupModel } from "../../models/purchaseGroup.model.js";
+import { PurchaseGroupRequestModel } from "../../models/purchaseGroupRequest.model.js";
+import { UserModel } from "../../models/user.model.js";
+
+const populatePurchaseGroup = [
+  {
+    path: "members",
+    select: "name username email _id",
+  },
+  {
+    path: "purchaseGroupRequests",
+    populate: {
+      path: "user",
+      model: "User",
+      select: "name username email _id",
+    },
+    select: "priceToInvest description status",
+  },
+];
+
+const findPurchaseGroupById = async (id) => {
+  return await PurchaseGroupModel.findById(id).populate(populatePurchaseGroup);
+};
 
 export const getPurchaseGroups = async (req, res, next) => {
-    try {
-        const purchaseGroups = await PurchaseGroupModel.find({});
+  try {
+    const purchaseGroups = await PurchaseGroupModel.find({});
+    return res.json(purchaseGroups);
+  } catch (error) {
+    next(error);
+  }
+};
 
-        return res.json(purchaseGroups);
-    } catch (error) {
-        next(error)
+export const getPurchaseGroupById = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const purchaseGroup = await findPurchaseGroupById(id);
+
+    if (!purchaseGroup) {
+      return res.status(404).json({ message: "Purchase group not found" });
     }
+
+    return res.json(purchaseGroup);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const createPurchaseGroup = async (req, res) => {
-    try {
-      // Create a new PurchaseGroupModel instance based on the request body
-      const newPurchaseGroup = new PurchaseGroupModel(req.body);
-  
-      // Save the new purchaseGroup to the database
-      const savedPurchaseGroup = await newPurchaseGroup.save();
-  
-      res.status(201).json(savedPurchaseGroup);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  };
+  try {
+    const purchaseGroupModelData = {
+      ...req.body,
+      members: [req.user._id],
+    };
 
-export const deletePurchaseGroup = async (req, res) => {
-    const purchaseGroupId = req.params.id;
-  
-    try {
-      const deletedPurchaseGroup = await PurchaseGroupModel.findByIdAndDelete(
-        purchaseGroupId,
-        req.body,
-        { new: true }
-      );
-  
-      if (!deletedPurchaseGroup) {
-        return res.status(404).json({ error: 'PurchaseGroup not found' });
-      }
-  
-      res.status(200).json(deletedPurchaseGroup);
-    } catch (error) {
-      console.error('Error deleting purchaseGroup:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  };
+    const newPurchaseGroup = new PurchaseGroupModel(purchaseGroupModelData);
+    const savedPurchaseGroup = await newPurchaseGroup.save();
+
+    res.status(201).json(savedPurchaseGroup);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 export const updatePurchaseGroup = async (req, res) => {
-    const purchaseGroupId = req.params.id;
-  
-    try {
-      const updatedPurchaseGroup = await PurchaseGroupModel.findByIdAndUpdate(
-        purchaseGroupId,
+  const purchaseGroupId = req.params.id;
+
+  try {
+    const updatedPurchaseGroup = await PurchaseGroupModel.findByIdAndUpdate(
+      purchaseGroupId,
+      req.body,
+      { new: true }
+    );
+
+    if (!updatedPurchaseGroup) {
+      return res.status(404).json({ error: "PurchaseGroup not found" });
+    }
+
+    res.status(200).json(updatedPurchaseGroup);
+  } catch (error) {
+    console.error("Error updating purchaseGroup:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const deletePurchaseGroup = async (req, res) => {
+  const purchaseGroupId = req.params.id;
+
+  try {
+    const deletedPurchaseGroup = await PurchaseGroupModel.findByIdAndDelete(
+      purchaseGroupId
+    );
+
+    if (!deletedPurchaseGroup) {
+      return res.status(404).json({ error: "PurchaseGroup not found" });
+    }
+
+    res.status(200).json(deletedPurchaseGroup);
+  } catch (error) {
+    console.error("Error deleting purchaseGroup:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getPurchaseGroupRequests = async (req, res) => {
+  try {
+    const { userId, groupId } = req.query;
+
+    let query = {};
+
+    if (userId) {
+      query.user = userId;
+    }
+
+    if (groupId) {
+      query.group = groupId;
+    }
+
+    const requests = await PurchaseGroupRequestModel.find(query).exec();
+
+    if (!requests.length) {
+      return res.status(404).json({ message: "No requests found" });
+    }
+
+    res.status(200).json(requests);
+  } catch (error) {
+    console.error("Error fetching purchase group requests:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const createPurchaseGroupRequest = async (req, res) => {
+  try {
+    const { _id: userId } = req.user;
+    const { group: groupId } = req.body;
+
+    const group = await PurchaseGroupModel.findById(groupId);
+
+    if (!group) {
+      return res.status(404).json({ error: "PurchaseGroup not found" });
+    }
+
+    const isMember = group.members.some((member) => member.equals(userId));
+
+    if (isMember) {
+      return res
+        .status(400)
+        .json({ error: "User is already a member of the group" });
+    }
+
+    const existingRequest = await PurchaseGroupRequestModel.findOne({
+      user: userId,
+      group: groupId,
+    });
+
+    if (existingRequest) {
+      return res.status(400).json({ error: "Request already exists" });
+    }
+
+    const groupRequestData = {
+      ...req.body,
+      user: userId,
+    };
+
+    const newGroupRequest = new PurchaseGroupRequestModel(groupRequestData);
+    const savedGroupRequest = await newGroupRequest.save();
+
+    await UserModel.findByIdAndUpdate(
+      userId,
+      { $push: { purchaseGroupRequests: savedGroupRequest._id } },
+      { new: true, useFindAndModify: false }
+    );
+
+    await PurchaseGroupModel.findByIdAndUpdate(
+      groupId,
+      { $push: { purchaseGroupRequests: savedGroupRequest._id } },
+      { new: true, useFindAndModify: false }
+    );
+
+    res.status(201).json(savedGroupRequest);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const updatePurchaseGroupRequest = async (req, res) => {
+  const groupRequestId = req.params.id;
+
+  try {
+    const updatedGroupRequest =
+      await PurchaseGroupRequestModel.findByIdAndUpdate(
+        groupRequestId,
         req.body,
         { new: true }
       );
-  
-      if (!updatedPurchaseGroup) {
-        return res.status(404).json({ error: 'PurchaseGroup not found' });
-      }
-  
-      res.status(200).json(updatedPurchaseGroup);
-    } catch (error) {
-      console.error('Error updating purchaseGroup:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+
+    if (!updatedGroupRequest) {
+      return res.status(404).json({ error: "GroupRequest not found" });
     }
-  };
+
+    res.status(200).json(updatedGroupRequest);
+  } catch (error) {
+    console.error("Error updating groupRequest:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const deletePurchaseGroupRequest = async (req, res) => {
+  const groupRequestId = req.params.id;
+
+  try {
+    const deletedGroupRequest =
+      await PurchaseGroupRequestModel.findByIdAndDelete(groupRequestId);
+
+    if (!deletedGroupRequest) {
+      return res.status(404).json({ error: "GroupRequest not found" });
+    }
+
+    res.status(200).json(deletedGroupRequest);
+  } catch (error) {
+    console.error("Error deleting groupRequest:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const changeRequestStatus = async (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+
+  try {
+    const updatedRequest = await PurchaseGroupRequestModel.findById(id);
+
+    if (!updatedRequest) {
+      return res.status(404).json({ error: "PurchaseGroupRequest not found" });
+    }
+
+    const previousStatus = updatedRequest.status;
+    updatedRequest.status = status;
+    await updatedRequest.save();
+
+    let group;
+
+    if (status === "approved" && previousStatus === "pending") {
+      group = await PurchaseGroupModel.findByIdAndUpdate(
+        updatedRequest.group,
+        { $addToSet: { members: updatedRequest.user } },
+        { new: true }
+      ).populate(populatePurchaseGroup);
+    } else if (status === "rejected" && previousStatus === "pending") {
+      group = await PurchaseGroupModel.findById(updatedRequest.group).populate(
+        populatePurchaseGroup
+      );
+    }
+
+    if (!group) {
+      return res.status(404).json({ error: "PurchaseGroup not found" });
+    }
+
+    res.status(200).json(group);
+  } catch (error) {
+    console.error("Error updating purchaseGroupRequest:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- server/src/api/purchaseGroups/purchaseGroup.router.js
+++ server/src/api/purchaseGroups/purchaseGroup.router.js
@@ -1,12 +1,31 @@
-import { AsyncRouter } from 'express-async-router';
-import { createPurchaseGroup, getPurchaseGroups, deletePurchaseGroup, updatePurchaseGroup } from './purchaseGroup.controller.js';
+import { AsyncRouter } from "express-async-router";
+import {
+  createPurchaseGroup,
+  getPurchaseGroups,
+  getPurchaseGroupById,
+  deletePurchaseGroup,
+  updatePurchaseGroup,
+  createPurchaseGroupRequest,
+  getPurchaseGroupRequests,
+  deletePurchaseGroupRequest,
+  updatePurchaseGroupRequest,
+  changeRequestStatus
+} from "./purchaseGroup.controller.js";
 import { protect } from "../../middlewares/auth.middleware.js";
 
 const router = AsyncRouter();
 
-router.get('/', getPurchaseGroups);
-router.post('/', protect, createPurchaseGroup);
-router.put('/:id', protect, updatePurchaseGroup);
-router.delete('/:id', protect, deletePurchaseGroup);
+router.get("/", getPurchaseGroups);
+router.get("/:id", getPurchaseGroupById);
+router.post("/", protect, createPurchaseGroup);
+router.put("/:id", protect, updatePurchaseGroup);
+router.delete("/:id", protect, deletePurchaseGroup);
+
+router.get("/purchaseGroupRequests", getPurchaseGroupRequests);
+router.post("/purchaseGroupRequest", protect, createPurchaseGroupRequest);
+router.put("/purchaseGroupRequest/:id", protect, updatePurchaseGroupRequest);
+router.delete("/purchaseGroupRequest/:id", protect, deletePurchaseGroupRequest);
+
+router.put("/changeRequestStatus/:id", protect, changeRequestStatus);
 
 export default router;
