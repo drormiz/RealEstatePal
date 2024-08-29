e9a4347443247e4d4ae928f21e1faf1a6cd6b461
MantinAlon <mantinalon5@gmail.com>
2024-08-29 17:12:38 +0300

    added host

--- server/src/index.js
+++ server/src/index.js
@@ -3,13 +3,16 @@ import { connectToDatabase } from './config/db.js';
 import { expressApp } from './config/express.js';
 
 const port = process.env.PORT || 3000;
+const host = process.env.HOST || '0.0.0.0';
 
 connectToDatabase()
   .then(() => {
     const app = expressApp();
 
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
+    app.listen(port, host, () => {
+      console.log(
+        `Server is running on http://${host}:${port}, host: ${host}, port: ${port}`
+      );
     });
   })
   .catch((error) => {
