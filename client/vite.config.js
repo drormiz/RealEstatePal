2a3e48508fc594c80d38eee3d89f65bef898523d
RazGavrielColman <raz5338@cs.colman.ac.il>
2024-08-30 13:46:19 +0300

    Update Vite configuration for external dependencies and server settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['@mui/icons-material/Home'],
+    },
+    outDir: 'dist',
+  },
   plugins: [react()],
-})
+  loader: { '.js': 'jsx' },
+  define: {
+    global: 'globalThis',
+  },
+  server: {
+    port: 80,
+    proxy: {
+      '/api': 'http://realestatepal.cs.colman.ac.il',
+      '/uploads': 'http://realestatepal.cs.colman.ac.il',
+    },
+  },
+});
